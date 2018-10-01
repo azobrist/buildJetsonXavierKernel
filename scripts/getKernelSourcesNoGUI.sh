b9b7cb40c278f652db02a81e0835af57f1c7cf95
anakin0620 <anakin0620@gmail.com>
2018-10-01 18:52:23 -0400

    Update getKernelSourcesNoGUI.sh

--- scripts/getKernelSourcesNoGUI.sh
+++ scripts/getKernelSourcesNoGUI.sh
@@ -3,12 +3,14 @@ apt-add-repository universe
 apt-get update
 apt-get install pkg-config -y
 cd /usr/src
-wget -N https://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.1/public_sources.tbz2
+wget -N https://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.1/public_sources.tbz2    
+# Here I have the question, I don't know which file I should download and where???
+
 sudo tar -xvf public_sources.tbz2 public_release/kernel_src.tbz2
 tar -xvf public_release/kernel_src.tbz2
 # Space is tight; get rid of the compressed kernel source
 rm -r public_release
-cd kernel/kernel-4.4
+cd kernel/kernel-4.9
 # Go get the default config file; this becomes the new system configuration
 zcat /proc/config.gz > .config
 
